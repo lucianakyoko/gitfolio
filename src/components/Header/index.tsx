cb3345b634d2194ea05a7ef4d4965e2cc7bc0f1b
Luciana Kyoko <lukyoko@protonmail.com>
2025-04-18 15:14:12 -0300

    feat(components): create Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,23 @@
+import { Avatar, AvatarImage } from "../ui/avatar";
+
+
 export function Header() {
   return (
-    <header className="p-3 bg-blue-100">
-      <span>Gitfolio</span>
-      <span>Docs</span>
+    <header className="py-3 px-6 bg-blue-50 flex justify-between">
+      <div className="flex items-center gap-6">
+        <a className='text-blue-950 font-bold text-xl' href="/">GitFolio</a>
+        <a className='text-blue-950 font-bold text-xl' href="/docs">Docs</a>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Avatar>
+          <AvatarImage           
+            src="https://avatars.githubusercontent.com/u/69259218?v=4" 
+            alt="Foto do perfil no Github"
+          /> 
+        </Avatar>
+        <span className="text-blue-950 font-bold text-sm">Luciana Kyoko</span>
+      </div>
     </header>
   )
 }

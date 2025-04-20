25c71be2ee22382cecc393063c8394b1ed4fd7ea
Luciana Kyoko <lukyoko@protonmail.com>
2025-04-20 14:49:04 -0300

    feat(components/Header): Display GitHub name and avatar using user context

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,9 @@
+import { useUser } from "@/contexts/UserContext";
 import { Avatar, AvatarImage } from "../ui/avatar";
 
-
 export function Header() {
+  const { user } = useUser()
+
   return (
     <header className="py-3 px-6 flex justify-between border-b border-b-blue-50">
       <div className="flex items-center gap-6">
@@ -9,15 +11,19 @@ export function Header() {
         <a className='text-blue-950 font-bold text-xl' href="/docs">Docs</a>
       </div>
 
-      <div className="flex items-center gap-2">
-        <Avatar>
-          <AvatarImage           
-            src="https://avatars.githubusercontent.com/u/69259218?v=4" 
-            alt="Foto do perfil no Github"
-          /> 
-        </Avatar>
-        <span className="text-blue-950 font-bold text-sm">Luciana Kyoko</span>
-      </div>
+      {
+        user && (
+        <div className="flex items-center gap-2">
+          <Avatar>
+            <AvatarImage           
+              src={user.avatar_url}
+              alt={`Foto de ${user.name} no Github`}
+            /> 
+          </Avatar>
+          <span className="text-blue-950 font-bold text-sm">{user.name}</span>
+        </div>
+        )
+      }
     </header>
   )
 }

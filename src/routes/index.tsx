d0846f6ad47ab33d9b01544e608c4c87a3defd09
Luciana Kyoko <lukyoko@protonmail.com>
2025-04-21 13:22:54 -0300

    feat(components/templates/MinimalTemplate): create "Minimal" theme layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { HomePage } from '../pages/HomePage'
 import { DocumentationPage } from '../pages/DocumentationPage'
+import { MinimalTemplate } from '../components/templates/MinimalTemplate'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/docs' element={<DocumentationPage />} />
+      <Route path='/user-name' element={<MinimalTemplate />} />
     </Routes>
   )
 }

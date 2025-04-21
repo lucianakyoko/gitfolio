627901185dd3b248c9f5d39bf16735cef1352f27
Luciana Kyoko <lukyoko@protonmail.com>
2025-04-21 16:08:12 -0300

    feat(components/templates/CreativeTemplate): create "Creative" theme layout

---
+++ src/components/templates/CreativeTemplate/index.tsx
@@ -0,0 +1,23 @@
+import { AboutSection } from "./components/AboutSection";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { HighlightProjectsSection } from "./components/HighlightProjectsSection";
+import { OtherProjectsSection } from "./components/OtherProjectsSection";
+import { SocialLinksSection } from "./components/SocialLinksSection";
+import { TecnologiesSection } from "./components/TecnologiesSection";
+
+export function CreativeTemplate() {
+  return (
+    <div className="bg-gray-100 pb-10">
+      <Header />
+      <main className="container max-w-5xl mx-auto px-6 py-16 space-y-20">
+        <SocialLinksSection />
+        <AboutSection />
+        <TecnologiesSection />
+        <HighlightProjectsSection />
+        <OtherProjectsSection />
+      </main>
+      <Footer />
+    </div>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { HomePage } from '../pages/HomePage'
 import { DocumentationPage } from '../pages/DocumentationPage'
-import { ModernTemplate } from '../components/templates/ModernTemplate'
+import { CreativeTemplate } from '../components/templates/CreativeTemplate'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/docs' element={<DocumentationPage />} />
-      <Route path='/user-name' element={<ModernTemplate />} />
+      <Route path='/user-name' element={<CreativeTemplate />} />
     </Routes>
   )
 }

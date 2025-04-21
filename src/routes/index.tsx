5a6d3b75b89c59dc156e0e33a759168c35b60ba0
Luciana Kyoko <lukyoko@protonmail.com>
2025-04-21 17:09:38 -0300

    feat(components/templates/ProfessionalTemplate): create "Professional" theme layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { HomePage } from '../pages/HomePage'
 import { DocumentationPage } from '../pages/DocumentationPage'
-import { CreativeTemplate } from '../components/templates/CreativeTemplate'
+import { ProfessionalTemplate } from '../components/templates/ProfessionalTemplate'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/docs' element={<DocumentationPage />} />
-      <Route path='/user-name' element={<CreativeTemplate />} />
+      <Route path='/user-name' element={<ProfessionalTemplate />} />
     </Routes>
   )
 }

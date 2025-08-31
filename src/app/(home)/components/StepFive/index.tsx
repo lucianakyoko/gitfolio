35838e5a3e4446aeb1602b1fa0cabda73a415870
Luciana Kyoko <lukyoko@protonmail.com>
2025-08-31 15:22:03 -0300

    feat(home/components): Add Step five component

--- src/app/(home)/components/StepFive/index.tsx
+++ src/app/(home)/components/StepFive/index.tsx
@@ -1,25 +1,61 @@
-import { CircleCheckBig } from "lucide-react";
-import { CreateRepoSection } from "./sections/CreateRepoSection";
-import { CopyContentSection } from "./sections/CopyContentSection";
-import { CustomizeRepoSection } from "./sections/CustomizeRepoSection";
-import { PortfolioUrlDisplaySection } from "./sections/PortfolioUrlDisplaySection";
+'use client'
 
-export function StepFive() {
-  return (
-    <div className="py-12 flex flex-col gap-12 items-center">
+import { ArrowRight, Download, Palette, SquarePen } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { PortfolioRenderer } from "@/components/PortfolioRenderer";
+import { useTheme } from "@/contexts/ThemeContext";
+
+type StepFoureProps = {
+  setStep: (number: number) => void
+}
+
+export function StepFive({ setStep }: StepFoureProps) {
+  const { theme } = useTheme();
+
+  return(
+    <div className="py-12 flex flex-col gap-7 items-center">
       <div className="flex flex-col gap-2 items-center">
-        <span className="bg-emerald-100 rounded-full p-3">
-          <CircleCheckBig size={48} color="#34D399"/>
-        </span>
-        <h2 className="text-3xl font-bold text-gray-600">Seu portfólio está pronto para ser criado!</h2>
-        <p className="text-xl text-gray-600">Siga estes passos para publicar seu portfólio GitFolio no GitHub:</p>
+        <h2 className="text-3xl font-bold text-gray-600">Preview</h2>
+        <div className="flex items-center flex-wrap gap-4">
+          <Button 
+            onClick={() => setStep(2)}
+            type="button" 
+            className="bg-white text-gray-600 border border-blue-100 hover:bg-blue-100 cursor-pointer"
+          >
+            <Palette />
+            Escolher outro tema
+          </Button>
+          
+          <Button 
+            onClick={() => setStep(3)}
+            type="button" 
+            className="bg-white text-gray-600 border border-blue-100 hover:bg-blue-100 cursor-pointer"
+          >
+            <SquarePen />
+            Editar
+          </Button>
+
+          <Button 
+            type="button" 
+            className="bg-white text-gray-600 border border-blue-100  hover:bg-blue-100 cursor-pointer"
+            onClick={() => console.log('baixar configurações')}
+          >
+            <Download />
+            Baixar Configurações
+          </Button>
+          <Button 
+            type="submit" 
+            className="bg-blue-500 text-white hover:bg-blue-600 cursor-pointer"
+            onClick={() => setStep(5)}
+          >
+            <ArrowRight />
+            Gerar Portifólio
+          </Button>
+        </div>
       </div>
 
-      <div className="space-y-8 w-96 md:w-3xl flex flex-col gap-9 pb-12 items-center md:items-start px-4 md:px-6">
-        <CreateRepoSection />
-        <CopyContentSection />
-        <CustomizeRepoSection />
-        <PortfolioUrlDisplaySection />
+      <div className="w-full max-w-5xl h-[600px] overflow-y-auto overflow-x-hidden rounded-2xl border border-blue-100 p-4 bg-white shadow-md relative">
+        <PortfolioRenderer themeId={theme}/>
       </div>
     </div>
   )

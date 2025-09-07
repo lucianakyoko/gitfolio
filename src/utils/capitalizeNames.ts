b2823451f3e279b858e7172b11e9b1f675871fbd
Luciana Kyoko <lukyoko@protonmail.com>
2025-09-07 15:17:17 -0300

    feat(utils): add capitalize name function

---
+++ src/utils/capitalizeNames.ts
@@ -0,0 +1,10 @@
+export function capitalizeName(name: string): string {
+  return name
+    .replace(/[-_]/g, ' ')
+    .split(' ')
+    .map(word =>
+      word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    )
+    .join(' ');
+}
+
